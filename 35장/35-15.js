83c8103781096614da59d2b36cf7366e29845194
Bang Uhyeok <0105bang@gmail.com>
2024-01-31 18:37:30 +0900

    배열 복사

---
+++ 35장/35-15.js
@@ -0,0 +1,14 @@
+// 배열 복사
+// ES5
+var origin = [1, 2];
+var copy = origin.slice();
+
+console.log(copy); // [1, 2]
+console.log(copy === origin); // false
+
+// ES6
+const origin = [1, 2];
+const copy = [...origin];
+
+console.log(copy); // [1, 2]
+console.log(copy === origin); // false
