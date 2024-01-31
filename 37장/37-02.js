716400176ddb0497cadc11de1fddc257a653881a
Bang Uhyeok <0105bang@gmail.com>
2024-01-31 23:40:46 +0900

    Set 객체의 생성

---
+++ 37장/37-02.js
@@ -0,0 +1,9 @@
+/*
+Set 생성자 함수는 이터러블을 인수로 전달받아 Set 객체를 생성한다. 이때 이터러블의 중복된 값은 Set 객체에
+요소로 저장되지 않는다.
+*/
+const set1 = new Set([1, 2, 3, 3]);
+console.log(set1); // Set(3) {1, 2, 3}
+
+const set2 = new Set('hello');
+console.log(set2); // Set(4) {"h", "e", "l", "o"}
---
+++ 37장/37-03.js
@@ -0,0 +1,11 @@
+/*
+중복을 허용하지 않는 Set 객체의 특성을 활용하여 배열에서 중복된 요소를 제거할 수 있다.
+*/
+
+// 배열의 중복 요소 제거
+const uniq = array => array.filter((v, i, self) => self.indexOf(v) === i);
+console.log(uniq([2, 1, 2, 3, 4, 3, 4])); // [2, 1, 3, 4]
+
+// Set을 사용한 배열의 중복 요소 제거
+const uniq_ = array => [...new Set(array)];
+console.log(uniq([2, 1, 2, 3, 4, 3, 4])); // [2, 1, 3, 4]
